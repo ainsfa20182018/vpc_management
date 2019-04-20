ec5b67399fe289a1ac65a6b8523bb51b97c196e6
ainsfa20182018 <ainsfa20182018@gmail.com>
2019-04-20 16:59:32 -0500

    change

--- sg.tf
+++ sg.tf
@@ -10,13 +10,13 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = # "0.0.0.0/0"
+    cidr_blocks = # ["0.0.0.0/0"]
   }
      egress {
     from_port       = 0
     to_port         = 0
-    protocol        = "tcp"
-    cidr_blocks     = "0.0.0.0/0"
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
     
   }
 }
