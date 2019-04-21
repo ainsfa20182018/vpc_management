f0c7fdef851ebe407f4c594a6c4fb0e993c56211
ainsfa20182018 <ainsfa20182018@gmail.com>
2019-04-21 13:51:44 -0500

    updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-     bucket = "terraform-state-april-mohammed2"
+     bucket = "terraform-state-january-mohammed2"
      key = "infra.state"
      region = "eu-west-1" 
   }
