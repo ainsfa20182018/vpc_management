40ae95c0e65053df6ac468b464b263836d08566e
mohammed <45836778+ainsfa20182018@users.noreply.github.com>
2019-04-20 22:10:14 -0500

    Update rout_table.tf

--- rout_table.tf
+++ rout_table.tf
@@ -1,15 +1,15 @@
 resource "aws_route_table" "r" {
-  vpc_id = "${aws_vpv.dev.id}"
+  vpc_id = "${aws_vpc.dev.id}"
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.igw1.id}"
   }
-    tags {
-    Name = "terraform-january-${var.Created_by}-${count.index +1}"
-    Env  = "${var.Env}"
-    Dept = "${var.Dept}"
-    Created_by = "${var.Created_by}"
+  tags {
+     Name = "terraform-january-${var.Created_by}-${count.index +1}"
+     Env  = "${var.Env}"
+     Dept = "${var.Dept}"
+     Created_by = "${var.Created_by}"
   }
 }
   
