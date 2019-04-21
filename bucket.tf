b54ab04575ff375cc58e25095a31cddcaf9177b3
ainsfa20182018 <ainsfa20182018@gmail.com>
2019-04-21 13:37:01 -0500

    add

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,6 @@
   
 resource "aws_s3_bucket" "newbucket" {
-  bucket = "terraform-state-april-mohammed11"
+  bucket = "terraform-state-april-mohammed1"
 
   tags {
      Name = "terraform-state-april-mohammed1" 
