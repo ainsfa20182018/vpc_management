1a2fbd133fa6867b47ae74a841263ebf1204fbc3
mohammed <45836778+ainsfa20182018@users.noreply.github.com>
2019-04-20 23:11:40 -0500

    Update backent.tf

--- backent.tf
+++ backent.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-     bucket = "terraform-state-january-farrukh1"
+     bucket = "terraform-state-january-mohammed"
      key = "infra.state"
      region = "eu-west-1" 
   }
