8a377f528fff2c5be59f4368c7a9554223ee64db
ainsfa20182018 <ainsfa20182018@gmail.com>
2019-04-21 12:25:19 -0500

    add

--- ig.tf
+++ ig.tf
@@ -1,5 +1,5 @@
 resource "aws_internet_gateway"  "igw1" {
-    vpc_id = "${vars_vpc.dev.id}"
+    vpc_id = "${aws_vpc.dev.id}"
     tags {
     Name = "terraform-january-${var.Created_by}-${count.index +1}"
     Env  = "${var.Env}"
