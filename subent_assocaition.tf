c4c96e9a3c31ec5bba52b4ee11d4435279c0f3d5
mohammed <45836778+ainsfa20182018@users.noreply.github.com>
2019-04-20 22:13:54 -0500

    Update subent_assocaition.tf

--- subent_assocaition.tf
+++ subent_assocaition.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "a" {
-  subnet_id      = "${aws_subnet.Dev1.id}"
-  route_table_id = "${aws_route_table.Dev1.id}"
+  subnet_id      = "${aws_subnet.dev1.id}"
+  route_table_id = "${aws_route_table.r.id}"
 }
