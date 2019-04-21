e12ba124a44f5134c480ce7f86713b31d4b0ca9f
mohammed <45836778+ainsfa20182018@users.noreply.github.com>
2019-04-20 22:02:10 -0500

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,11 @@
   
-resource "aws_s3_bucket" "newbucket" { 
-
+resource "aws_s3_bucket" "newbucket" {
   bucket = "terraform-january-mohammed"
+
+  tags {
+     Name = "terraform-january-mohammed" 
+     Env  = "dev" 
+     Dept = "IT" 
+     Created_by = "mohammed"
+  }
 }
